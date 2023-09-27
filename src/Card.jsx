23f7004d218654829125f68a7d159d9fa44d61a8
unknown <thandodidiza197@gmail.com>
2023-09-27 13:49:22 +0200

    added icons

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,10 +8,10 @@ const Card = ({ title, info }) => {
         <article className='w-full rounded-lg px-4 border shadow-xl border-gray-400'>
             <div className="flex justify-between items-center py-4">
                 <h2 className="text-base md:text-2xl font-semibold text-black/80">{title}</h2>
-                <span onClick={() => setShow(!show)} className="bg-slate-500 p-2 md:p-4 rounded-full"><FaPlus className='text-pink-700' /></span>
+                <span onClick={() => setShow(!show)} className="bg-slate-500 p-2 md:p-4 rounded-full">{show ? <FaMinus className='text-pink-700' />:<FaPlus className='text-pink-700' />}</span>
             </div>
 
-            {show && <p className="text-base duration-700 my-4 font-light text-gray-500">{info}</p>}
+            {show && <p className="text-base  my-4 font-light text-gray-500">{info}</p>}
 
         </article>
     )
